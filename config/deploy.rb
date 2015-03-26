64d1b8bc1dde535a4010229cf048b22d497b116d
Rodrigo <denoww@gmail.com>
2015-03-26 08:26:35 -0300

    Capstrano vai sempre fazer deploy do master (não perguntar mais)

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'sc_redmine'
 set :repo_url, 'git@github.com:denoww/redmine-sc.git'
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/sc_redmine'
